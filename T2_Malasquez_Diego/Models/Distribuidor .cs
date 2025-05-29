71e3645f59b074f2a4c69ba9a3c3ea4e1558b759
upn_i <upn_i@DESKTOP-1KG2GS2>
2025-05-29 04:25:06 +0200

    Agregar archivos de proyecto.

---
+++ T2_Malasquez_Diego/Models/Distribuidor .cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace T2_Malasquez_Diego.Models
+{
+    public class Distribuidor
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        public string NombreDistribuidor { get; set; }
+        public string RazonSocial { get; set; }
+        public int Telefono { get; set; }
+        public int AñoInicioOperacion { get; set; }
+
+        public string Contacto { get; set; }
+     }
+}
